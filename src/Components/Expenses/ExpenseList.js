a218b1f045c836517a11b81270d432666621760f
KrishnaMurar <erkmtulu@gmail.com>
2023-04-30 14:11:38 +0530

    filtered array_Single

---
+++ src/Components/Expenses/ExpenseList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './ExpenseList.css';
+
+
+const ExpenseList = props => {
+ if(props.items.length ===0){
+    return <h2 className ='expense-list__fallback'>No Result Found</h2>
+ }
+ if(props.items.length ===1){
+    return ( <div>
+    <ul className = 'expense-list'>
+    {props.items.map((expenses) => (
+<ExpenseItem key ={expenses.id} title = {expenses.title} Amount = {expenses.Amount} Date = {expenses.Date} />
+
+)
+
+)}
+</ul>
+<h2 className ='expense-list__fallback'>Only single Expense here. Please add more...</h2></div>)
+ }
+    return (
+        <ul className = 'expense-list'>
+                {props.items.map((expenses) => (
+        <ExpenseItem key ={expenses.id} title = {expenses.title} Amount = {expenses.Amount} Date = {expenses.Date} />
+         
+        )
+      
+  )}
+        </ul>
+    )
+}
+
+export default ExpenseList;
